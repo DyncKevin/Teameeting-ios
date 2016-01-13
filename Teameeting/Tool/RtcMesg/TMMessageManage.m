48eed76fd0c3ff5e102efe9dcff03b04d6eb7e74
wangyangyang@dync.cc <wangyangyang@dync.cc>
2016-01-13 19:54:48 +0800

    修改视频订阅参数

--- Teameeting/Tool/RtcMesg/TMMessageManage.m
+++ Teameeting/Tool/RtcMesg/TMMessageManage.m
@@ -368,7 +368,7 @@
                     
                     if ([object respondsToSelector:@selector(videoSubscribeWith:)] && [object receiveMessageEnable]) {
                         
-                        [object videoSubscribeWith:[messageDic objectForKey:@"room"]];
+                        [object videoSubscribeWith:[messageDic objectForKey:@"cont"]];
                     }
                 }
                 return;
@@ -404,7 +404,7 @@
 
 - (void) OnMsgServerConnected {
     
-    
+    NSLog(@"ok");
 }
 
 - (void) OnMsgServerDisconnect {
