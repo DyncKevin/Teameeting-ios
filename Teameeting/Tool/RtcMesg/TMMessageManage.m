c06aa7428c970e16280d18d55451082d9ba66131
dync <zhengxiaokai@dync.cc>
2016-01-27 16:12:52 +0800

    add params nick name and room name
    add params nick name and room name

--- Teameeting/Tool/RtcMesg/TMMessageManage.m
+++ Teameeting/Tool/RtcMesg/TMMessageManage.m
@@ -53,7 +53,7 @@
 
 - (void)inintTMMessage {
     //192.168.7.39 :6630      180.150.179.128  :6630
-    [_msg tMInitMsgProtocol:self uid:[SvUDIDTools UDID] token:[ServerVisit shead].authorization server:TMMessageUrl port:6630];
+    [_msg tMInitMsgProtocol:self uid:[SvUDIDTools UDID] token:[ServerVisit shead].authorization nname:@"nick name" server:TMMessageUrl port:6630];
 }
 
 - (void)registerMessageListener:(id<tmMessageReceive>)listener {
@@ -345,17 +345,17 @@
 
 - (int)sendMsgWithRoomid:(NSString *)roomid msg:(NSString *)msg {
     
-   return [self.msg tMSndMsgRoomid:roomid msg:msg];
+   return [self.msg tMSndMsgRoomid:roomid rname:@"room name" msg:msg];
 }
 
 - (int)tmRoomCmd:(MCMeetCmd)cmd roomid:(NSString *)roomid remain:(NSString *)remain {
 
-    return [self.msg tMOptRoomCmd:cmd roomid:roomid remain:remain];
+    return [self.msg tMOptRoomCmd:cmd roomid:roomid rname:@"room name" remain:remain];
 }
 
 - (int)tMNotifyMsgRoomid:(NSString*)roomid withTags:(MCSendTags)tags withMessage:(NSString*)meg
 {
-    return [self.msg tMNotifyMsgRoomid:roomid tags:tags msg:meg];
+    return [self.msg tMNotifyMsgRoomid:roomid rname:@"room name" tags:tags msg:meg];
 }
 
 //接收消息
